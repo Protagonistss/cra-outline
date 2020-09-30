a34c29d77661f08d50171969a21ef45da8726cb5
protagonisths <13837050616@163.com>
2020-10-01 01:17:07 +0800

    feature: add iconfont

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./static/js/lib/flexible";
+import "./static/style/iconfont/iconfont.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
