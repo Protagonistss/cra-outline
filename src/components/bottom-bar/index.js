140d5c31413c56e7c5cb6497a3ae2de04d6986d8
protagonisths <13837050616@163.com>
2020-10-01 01:31:37 +0800

    feature: add sass

--- src/components/bottom-bar/index.js
+++ src/components/bottom-bar/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { MenuModel } from "../../constants/custom";
+import "./index.scss";
 
 const model = new MenuModel();
 export default class BottonBar extends Component {
   render() {
     return (
-      <ul className="bottonNav">
+      <ul className="bottomBar">
         {model.getData.map((item) => {
           return <MenuItem key={item.key} {...item}></MenuItem>;
         })}
